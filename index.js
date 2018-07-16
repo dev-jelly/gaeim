da1f78c080f193da1a9685e7c27f02817e0d3c17
myoungho.pak <qkraudghgh@gmail.com>
2018-07-16 09:45:21 +0900

    split js file

---
+++ index.js
@@ -0,0 +1,50 @@
+const createDivWithClass = className => {
+  const div = document.createElement("div");
+  div.className = className;
+  return div;
+};
+
+const createPlatformLine = (platform, value) =>
+  `<p><span class="community-title ${platform}">${platform}:</span> ${value}</p>`;
+
+const addAccountInfoDOM = account => {
+  const accountEl = createDivWithClass("account");
+
+  // thumbnail 추가
+  if (account.thumbnail) {
+    accountEl.innerHTML = `<img class="account-thumbnail" src="${
+      account.thumbnail
+    }">`;
+  }
+
+  const accountInfoEl = createDivWithClass("account-info");
+
+  // nickname 추가
+  accountInfoEl.innerHTML += `<p class="nickname">${account.nickname}</p>`;
+
+  // platform info 추가
+  for (const platform in account) {
+    if (platform !== "thumbnail" && platform !== "nickname") {
+      accountInfoEl.innerHTML += createPlatformLine(
+        platform,
+        account[platform]
+      );
+    }
+  }
+
+  accountEl.appendChild(accountInfoEl);
+
+  return accountEl;
+};
+
+fetch("http://jellyms.kr/gaeim/accounts.json")
+  .then(res => res.json())
+  .then(accounts => {
+    const accountsDOM = createDivWithClass("accounts");
+    const filteredAccounts = accounts.filter(account => "nickname" in account); // nickname 없는 data는 빼버림
+    filteredAccounts.forEach(account => {
+      accountsDOM.appendChild(addAccountInfoDOM(account));
+    });
+
+    document.body.appendChild(accountsDOM);
+  });
